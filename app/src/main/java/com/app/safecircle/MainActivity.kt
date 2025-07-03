83e4572a3d1544b564e28d27f2a0b84609286ba4
Krish Bhavsar <krishbhavsar101@gmail.com>
2025-07-03 14:29:02 +0530

    init project

---
+++ app/src/main/java/com/app/safecircle/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.app.safecircle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+private fun MainActivity.openScreen() {
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottomBar)
+
+        bottomBar.setOnItemSelectedListener { menuItem ->
+
+            if (menuItem.itemId == R.id.nav_security) {
+                inflateSecurityFragment()
+            } else if (menuItem.itemId == R.id.nav_home) {
+                inflateHomeFragment()
+            } else if (menuItem.itemId == R.id.nav_dashboard) {
+                inflateDashboardFragment()
+            } else {
+                inflateProfileFragment()
+            }
+
+            true
+        }
+    }
+
+    private fun MainActivity.inflateDashboardFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, DashboardFragment.newInstance())
+        transaction.commit()
+    }
+
+    private fun MainActivity.inflateSecurityFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, SecurityFragment.newInstance())
+        transaction.commit()
+    }
+
+    private fun MainActivity.inflateHomeFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, HomeFragment.newInstance())
+        transaction.commit()
+    }
+
+    private fun MainActivity.inflateProfileFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, ProfileFragment.newInstance())
+        transaction.commit()
+    }
+}
---
+++ app/src/main/java/com/app/safecircle/SecurityScreen.kt
@@ -0,0 +1,4 @@
+package com.app.safecircle
+
+class SecurityScreen {
+}
